c6fbbc4f95f4ea1f12a0e796b67b265e401db6ec
MingZone <lanou3g@Ming.local>
2014-06-10 16:42:01 +0800

    1.0
    lalala

---
+++ MIng-hello/MIng-hello/MingAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MingAppDelegate.h
+//  MIng-hello
+//
+//  Created by Ming on 14-6-10.
+//  Copyright (c) 2014年 lanou3g.com 蓝欧科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MingAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MIng-hello/MIng-hello/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MIng-hello
+//
+//  Created by Ming on 14-6-10.
+//  Copyright (c) 2014年 lanou3g.com 蓝欧科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MingAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MingAppDelegate class]));
+    }
+}
